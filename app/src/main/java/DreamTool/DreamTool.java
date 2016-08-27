fdcf724c2cb2001011a1246ba8f113b59ce150b9
robertkeazor <robertskeazor@weddingwire.com>
2016-08-27 14:40:09 -0400

    Added some basic structure , depenedencies and a recyclerview adapter

---
+++ app/src/main/java/DreamTool/DreamTool.java
@@ -0,0 +1,32 @@
+package DreamTool;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.androidsummit2016.dreamon.R;
+import com.hookedonplay.decoviewlib.DecoView;
+
+import butterknife.BindView;
+
+public class DreamTool extends AppCompatActivity implements DreamToolView, DreamToolAdapter.OnDreamToolViewItemClick {
+    DreamToolPresenter mPresenter = new DreamToolPresenter();
+
+    @BindView(R.id.dreamChart) DecoView mDreamChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dream_tool);
+        mPresenter.setView(this);
+    }
+
+    @Override
+    public void renderDreamTool() {
+
+    }
+
+    @Override
+    public void onClick() {
+
+    }
+}
